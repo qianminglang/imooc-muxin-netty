e7d38ed7780ae8195603c3c57c7f5e7d180eed54
qianminglang <569040804@qq.com>
2019-07-06 16:26:51 +0800

    init

---
+++ imooc-muxin-netty/src/main/java/com/imooc/imoocmuxinnetty/ImoocMuxinNettyApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.imoocmuxinnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocMuxinNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocMuxinNettyApplication.class, args);
+    }
+
+}
